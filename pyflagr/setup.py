61bf04af9e6850b31d0f3beb43058ca5d60c2ab1
Leonidas Akritidis <45494898+lakritidis@users.noreply.github.com>
2022-06-21 18:48:13 +0300

    Add files via upload

--- pyflagr/setup.py
+++ pyflagr/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 DESCRIPTION = "PyFLAGR is a Python package for aggregating ranked preference lists from multiple sources."
-LONG_DESCRIPTION = "PyFLAGR is a Python package for aggregating ranked preference lists from multiple sources."
+LONG_DESCRIPTION = "FLAGR is a high performing, modular library for rank aggregation. To ensure the highest possible performance, the core FLAGR library is written in C++ and implements a wide collection of unsupervised rank aggregation methods. Its modular design allows third-party programmers to implement their own algorithms and easily rebuild the entire library. FLAGR can be built as a standard application, or as a shared library (so or dll). In the second case, it can be linked from other C/C++ programs, or even from programs written in other languages (e.g. Python, PHP, etc.). In this context, PyFLAGR is a Python library that links to FLAGR and allows a developer to exploit the efficient FLAGR implementations from a standard Python program."
 
 setup(
     name='pyflagr',
@@ -14,8 +14,10 @@ setup(
     author_email="lakritidis@ihu.gr",
     maintainer="Leonidas Akritidis",
     maintainer_email="lakritidis@ihu.gr",
-    packages=find_packages(),
-    install_requires=["pandas==1.4.2"],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/lakritidis/FLAGR',
+    install_requires=["pandas"],
     license="Apache",
     keywords=[
         "rank aggregation", "rank fusion", "data fusion", "unsupervised learning", "information retrieval",
