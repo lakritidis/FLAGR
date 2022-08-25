12a32d7c2e06a65bce8b6d33f7a5c24aa85b53b8
Leonidas Akritidis <45494898+lakritidis@users.noreply.github.com>
2022-08-26 02:00:19 +0300

    Delete MC123.cpp

--- src/ram/MC123.cpp
+++
@@ -1,9 +0,0 @@
-/// MC1, MC2, MC3: The first, second and third Markov Chains method of Dwork et. al, 2001. Published
-/// in:
-/// Dwork C., Kumar R., Naor M., Sivakumar D., "Rank Aggregation Methods for the Web", In
-///	Proceedings of the 10th International Conference on World Wide Web, pp. 613-622, 2001.
-/// ///////////////////////////////////////////////////////////////////////////////////////////////
-
-void MergedList::MC(class InputList ** inlists, class SimpleScoreStats * s, class InputParams * prms) {
-}
-
