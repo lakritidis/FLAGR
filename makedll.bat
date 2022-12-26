1a3f6bba84eb9cb8a718d633b62009b16acb1b41
Leonidas Akritidis <45494898+lakritidis@users.noreply.github.com>
2022-12-26 22:56:53 +0200

    Delete makedll.bat

--- makedll.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Program Files\CodeBlocks\MinGW\bin
-g++ -O3 -c -o C:/Users/Leo/Documents/cpp_algorithms/FLAGR/shared/flagr.o C:/Users/Leo/Documents/cpp_algorithms/FLAGR/dllflagr.cpp
-g++ -O3 -o C:/Users/Leo/Documents/cpp_algorithms/FLAGR/pyflagr/pyflagr/flagr.dll -s -shared C:/Users/Leo/Documents/cpp_algorithms/FLAGR/shared/flagr.o -Wl,--subsystem,windows
-cd C:\Users\Leo\Documents\cpp_algorithms\FLAGR
